f960129afb3bd6a1f6709e90b6d23afd6b198f90
vicasa_hw <vicasa@naver.com>
2021-05-20 18:07:42 +0900

    inside사진 변경

--- portfolios/Inside/js/script.js
+++ portfolios/Inside/js/script.js
@@ -1,27 +1,28 @@
-$(document).ready(function(){
+$(function(){ 
 	$('#main_navigation a').click(function(){
-		// ********
-		var target = $(this).attr('data-target');
-		$(':not([data-name='+target+'])').removeClass('active');
+		// **********
+		var target=$(this).attr('data-target');
+		$(':not([data-name'+target+'])').removeClass('active');
 		$('[data-name='+target+']').addClass('active');
 	});
 
+	// left btn
 	$('#character a:nth-child(1)').click(function(){
+		currentPosition-=1;
+		if(currentPosition<1){
+			currentPosition=3;
+		}
 		$('#character article').removeClass('show');
-		$().addClass('show');
+		$('article:nth-of-type('+currentPosition+')').addClass('show');
 	});
-
-
 	var currentPosition =1;
-
-	// right button
+	// right btn
 	$('#character a:nth-child(2)').click(function(){
-		curruentPosition+=1;
-		if (currentPosition>3) {
+		currentPosition+=1;
+		if(currentPosition>3){
 			currentPosition=1;
 		}
 		$('#character article').removeClass('show');
-		$('article :nth-of-type('+curruentPosition+')').addClass('show');
+		$('article:nth-of-type('+currentPosition+')').addClass('show');
 	});
-	
 });
